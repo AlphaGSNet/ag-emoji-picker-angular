96e69040f1100c056cb3c73b7040f0c5522c616c
Alexey <alexey.podobed@gmail.com>
2017-10-23 11:45:47 +0300

    added title for emoji icon

--- src/app/filters/imagify.filter.js
+++ src/app/filters/imagify.filter.js
@@ -8,7 +8,7 @@ export function imagifyFilter(EmojiPages) {
 
     return input.replace(regex, (match, text) => {
       let className = text.replace(/_/g, '-');
-      return `<img class="emoji emoji-${className}">`;
+      return `<img class="emoji emoji-${className}" title=":${className}:" alt="${className}">`;
     })
   };
 }
