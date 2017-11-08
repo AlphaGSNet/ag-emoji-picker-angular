7e7dc14ef5c327a139d7b9b4114cde89f5f48d7e
Alexey <alexey.podobed@gmail.com>
2017-11-08 11:11:43 +0200

    changed path to images

--- src/app/filters/imagify-single.js
+++ src/app/filters/imagify-single.js
@@ -8,10 +8,10 @@ export function imagifySingleFilter(EmojiPages, bfEmojiPickerUtils) {
     if (!input) return '';
 
     return input.replace(regex, (match, text) => {
-      return `<img src="/images/icons${isRetina ? '@80' : '@40'}/${text}.png"
+      return `<img src="../images/icons${isRetina ? '@80' : '@40'}/${text}.png"
                    class="emoji-single"
                    title=":${text}:"
                    alt="${text}">`;
-    })
+    });
   };
 }
